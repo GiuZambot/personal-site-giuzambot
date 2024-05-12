458e7cabb1693f6f317a27bbf70a845790ac1567
GiuZambot <giuzambot@gmail.com>
2024-05-12 16:05:11 -0300

    try: godot

--- src/widgets/GodotGame/GodotGame.tsx
+++ src/widgets/GodotGame/GodotGame.tsx
@@ -1,7 +1,9 @@
+import { IframeHTMLAttributes } from "react";
+
 const Game = () => {
   return (
     <div>
-      <iframe src="./game/index.html" width="100%" height="600" style={{ border: 'none' }}></iframe>
+      <iframe sandbox="cross-origin-isolated allow-same-origin allow-scripts" allow="cross-origin-isolated allow-same-origin allow-scripts" src="./game/index.html" width="100%" height="600" style={{ border: 'none' }}></iframe>
     </div>
   );
 };
