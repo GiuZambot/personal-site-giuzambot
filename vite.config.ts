030c150ca661437bfbc222f37dee0526b27dbf9d
GiuZambot <giuzambot@gmail.com>
2024-05-12 14:43:10 -0300

    feat: game 0.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  {
+    name: 'configure-response-headers',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+        next();
+      });
+    }
+  }],
+
 })
