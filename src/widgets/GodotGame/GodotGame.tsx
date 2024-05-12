48475f14fdd2e425f9c30c595f4766a5d272f55c
GiuZambot <giuzambot@gmail.com>
2024-05-12 16:26:16 -0300

    fix: url game

--- src/widgets/GodotGame/GodotGame.tsx
+++ src/widgets/GodotGame/GodotGame.tsx
@@ -1,7 +1,7 @@
 const Game = () => {
   return (
     <div>
-      <iframe sandbox="cross-origin-isolated allow-same-origin allow-scripts" allow="cross-origin-isolated allow-same-origin allow-scripts" src="./game/index.html" width="100%" height="600" style={{ border: 'none' }}></iframe>
+      <iframe allow="cross-origin-isolated allow-same-origin allow-scripts" src="./game/index.html" width="100%" height="600" style={{ border: 'none' }}></iframe>
     </div>
   );
 };
